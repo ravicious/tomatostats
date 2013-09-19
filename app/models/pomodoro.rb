e9b6eaf4bcff04a21c0b16e505b83b974707848f
Rafał Cieślak <ravicious@gmail.com>
2013-09-19 14:53:49 +0200

    Pomodoro#duration validation

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -1,6 +1,14 @@
 class Pomodoro < ActiveRecord::Base
+  validate :duration_cannot_be_longer_than_1_hour
 
   def duration
     Time.diff Time.at(started_at), Time.at(finished_at)
   end
+
+  def duration_cannot_be_longer_than_1_hour
+    if duration[:hour] > 1
+      errors.add(:duration, "can't be longer than 1 hour")
+    end
+  end
+
 end
