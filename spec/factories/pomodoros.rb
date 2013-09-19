56c8155647a465048d69a12571b01395e7ad77ca
Rafał Cieślak <ravicious@gmail.com>
2013-09-19 14:50:21 +0200

    Add :too_long_duration trait to pomodoro factory

--- spec/factories/pomodoros.rb
+++ spec/factories/pomodoros.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
   factory :pomodoro do
     sequence(:started_at) {|n| 1.day.ago.to_i + n*60*30}
     sequence(:finished_at) {|n| 1.day.ago.to_i + n*60*30 + 25*60}
+
+    trait(:too_long_duration) do
+    sequence(:started_at) {|n| 1.day.ago.to_i + n*60*30}
+    sequence(:finished_at) {|n| 1.day.ago.to_i + n*60*30 + 25*60 + 60*60*5}
+    end
   end
 end
