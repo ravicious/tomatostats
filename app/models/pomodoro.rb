f2b4aa6412fa110cb0fea837d5e498da4f2a94da
Rafał Cieślak <ravicious@gmail.com>
2013-09-20 13:44:18 +0200

    Validate numericality of pomodoro's :started_at and :finished_at fields

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -2,6 +2,7 @@ class Pomodoro < ActiveRecord::Base
   belongs_to :user
   validate :duration_cannot_be_longer_than_1_hour
   validates_uniqueness_of :started_at, :finished_at
+  validates_numericality_of :started_at, :finished_at
 
   def duration
     Time.diff Time.at(started_at), Time.at(finished_at)
