a2a155f1fe641afc739c9678baf0015c40b0d814
Rafał Cieślak <ravicious@gmail.com>
2013-09-20 14:00:58 +0200

    Rewrote ClockworkTomatoImporter

--- app/importers/clockwork_tomato_importer.rb
+++ app/importers/clockwork_tomato_importer.rb
@@ -1,7 +1,25 @@
-require 'singleton'
-
 class ClockworkTomatoImporter < Importer
-  include Singleton
 
   private
+
+  def custom_import
+    lines = []
+
+    IO.foreach(@input) do |line|
+      lines << line
+      if lines.size >= 100
+        parse_lines(lines)
+        lines = []
+      end
+    end
+
+    parse_lines(lines)
+  end
+
+  def parse_lines(lines)
+    CSV.parse(lines.join) do |row|
+      create_and_assign_pomodoro(started_at: row[5], finished_at: row[6])
+    end
+  end
+
 end
