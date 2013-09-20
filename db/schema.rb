815fbd66bd8e0d505b1721dc9da14e3311d94169
Rafał Cieślak <ravicious@gmail.com>
2013-09-20 09:55:49 +0200

    Generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919123725) do
+ActiveRecord::Schema.define(version: 20130920075200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20130919123725) do
   create_table "pomodoros", force: true do |t|
     t.integer  "started_at"
     t.integer  "finished_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
