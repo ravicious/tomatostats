78f3b55b603e07a3c03efa056c9d9b7df9e4cb0a
Rafał Cieślak <ravicious@gmail.com>
2013-09-20 14:00:47 +0200

    Rewrite Importer class

--- app/importers/importer.rb
+++ app/importers/importer.rb
@@ -1,29 +1,52 @@
-require 'singleton'
-
 class Importer
-  include Singleton
 
   Response = Struct.new(:success?, :pomodoros_added_count)
   FAIL_RESPONSE = Response.new(false, 0)
+
+  def initialize(input, user)
+    @input = input
+    @user = user
+    @response = nil
+  end
 
   def importers
     @@importers ||= Hash.new(EmptyImporter)
   end
 
   def response
-    @@response ||= nil || FAIL_RESPONSE
+    @response || FAIL_RESPONSE
   end
 
-  def import(input, user)
-    clear_response
-    read_input(input)
-    create_and_assign_pomodoros_to_user(user)
-    self
+  def import
+    begin
+      count_imported_pomodoros do
+        custom_import
+      end
+      set_success_response
+    rescue
+      set_fail_response
+    ensure
+      self
+    end
   end
 
   private
 
-  def clear_response
-    @@response = nil
+  def create_and_assign_pomodoro(args = {})
+    @user.pomodoros.find_or_create_by(args)
+  end
+
+  def count_imported_pomodoros(&block)
+    pomodoros_before = @user.pomodoros.count
+    yield
+    @pomodoros_count = @user.pomodoros.count - pomodoros_before
+  end
+
+  def set_success_response
+    @response = Response.new(true, @pomodoros_count)
+  end
+
+  def set_fail_response
+    @response = Response.new(false, 0)
   end
 end
