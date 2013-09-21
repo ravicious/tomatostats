9657e2bfc2984644459fcd6a344959d1e087b8e3
Rafał Cieślak <ravicious@gmail.com>
2013-09-21 21:49:48 +0200

    Show notice informing how many pomodoros have been imported

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -8,6 +8,11 @@ class ImportsController < ApplicationController
     file = params[:import][:file].path
     @import = Importer.importers[params[:import][:application]].new(input: file, user: current_user)
     @import.import
+    if @import.imported_pomodoros > 0
+      flash[:notice] = "#{TextHelper.pluralize(@import.imported_pomodoros, "pomodoro")} imported."
+    else
+      flash[:notice] = "Zero new pomodoros imported."
+    end
     redirect_to root_path
   end
 end
