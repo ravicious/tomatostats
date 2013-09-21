2ecf9c877bfc54bda631687c3b44b198eea0704a
Rafał Cieślak <ravicious@gmail.com>
2013-09-21 21:16:02 +0200

    Add format to Pomodoro#duration

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -4,8 +4,9 @@ class Pomodoro < ActiveRecord::Base
   validates_uniqueness_of :started_at, :finished_at, scope: :user
   validates_numericality_of :started_at, :finished_at
 
+  # TODO: rewrite this method
   def duration
-    Time.diff Time.at(started_at), Time.at(finished_at)
+    Time.diff Time.at(started_at), Time.at(finished_at), '%N %S'
   end
 
   def duration_cannot_be_longer_than_1_hour
