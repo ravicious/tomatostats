a75676ca17a66667022069bc3fa7b9014e6d8628
Rafał Cieślak <ravicious@gmail.com>
2013-09-21 19:38:28 +0200

    Slightly refactored imports controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -3,7 +3,7 @@ class ImportsController < ApplicationController
   end
 
   def create
-    file = params[:import][:file].tempfile
+    file = params[:import][:file].path
     @import = Importer.importers[params[:import][:application]].new(input: file, user: current_user)
     @import.import
     redirect_to root_path
