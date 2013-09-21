fc27dec94401f621dd2d20519c06a3fa858f0366
Rafał Cieślak <ravicious@gmail.com>
2013-09-21 14:31:37 +0200

    import_pomodoros Capybara helper

--- spec/support/capybara_helpers.rb
+++ spec/support/capybara_helpers.rb
@@ -1,4 +1,12 @@
 def sign_in
-  visit root_path
+  visit '/'
   click_link "with Facebook"
+end
+
+def import_pomodoros(input)
+  visit '/'
+  click_link "Import pomodoros"
+  attach_file "File", input
+  select "Clockwork Tomato", from: "Application"
+  click_button "Submit"
 end
