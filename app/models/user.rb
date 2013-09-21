bd0a919742452c2e3afab8dd592636e665029e6e
Rafał Cieślak <ravicious@gmail.com>
2013-09-21 11:37:07 +0200

    User.find_for_facebook_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   devise :rememberable, :trackable, :omniauthable,
          omniauth_providers: [:facebook]
   has_many :pomodoros
+
+  def self.find_for_facebook_oauth(access_token)
+    User.find_or_create_by(provider: access_token.provider, uid: access_token.uid) do |user|
+      user.name = access_token.extra.raw_info.username.presence || access_token.extra.raw_info.name
+    end
+  end
 end
