fc1ec49bcbbea272eab45f38db24b93a213eafb7
Rafał Cieślak <ravicious@gmail.com>
2013-09-22 11:39:45 +0200

    Complete projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,9 +2,23 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!
 
   expose(:projects) { current_user.projects }
-  expose(:project)
+  expose(:project, attributes: :project_params)
 
   def new
 
+  end
+
+  def create
+    if project.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
