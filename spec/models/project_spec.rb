853cdc7936b5f2cd2e208c911a2d0f7cff34edff
Rafał Cieślak <ravicious@gmail.com>
2013-09-22 11:57:28 +0200

    Fix project spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,13 +4,14 @@ describe Project do
   context "when trying to make two projects with the same name" do
     context "by the same user" do
       let(:user) { create(:user) }
+      let(:new_project) { build(:project, name: "Example project name", user: user) }
 
       before do
-        2.times { create(:project, name: "Example project name", user: user) }
+        create(:project, name: "Example project name", user: user)
       end
 
-      it "creates only one project" do
-        expect(user).to have(1).project
+      it "makes the second project not valid" do
+        expect(new_project).not_to be_valid
       end
     end
 
