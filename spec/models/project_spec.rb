13e05726d9641626adb25d522c412cf12f98e615
Rafał Cieślak <ravicious@gmail.com>
2013-09-22 11:53:09 +0200

    Add project spec
    Adding projects with the same name

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when trying to make two projects with the same name" do
+    context "by the same user" do
+      let(:user) { create(:user) }
+
+      before do
+        2.times { create(:project, name: "Example project name", user: user) }
+      end
+
+      it "creates only one project" do
+        expect(user).to have(1).project
+      end
+    end
+
+    context "by two different users" do
+      before do
+        2.times { create(:project, name: "Example project name") }
+      end
+
+      it "creates two projects" do
+        expect(Project.count).to eq(2)
+      end
+    end
+  end
 end
