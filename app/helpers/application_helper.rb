e9c65fe33e9e53b98c366751a92531d3fe1c5d5a
Rafał Cieślak <ravicious@gmail.com>
2013-09-23 16:27:52 +0200

    Show flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def glyph(name, white: false, space: true)
     content_tag(:i, nil, class: "icon-#{name}#{" icon-white" if white}") + (space ? " " : "")
   end
+
+  def flash_class(level)
+      case level
+          when :notice then "alert alert-info"
+          when :success then "alert alert-success"
+          when :error then "alert alert-warning"
+          when :alert then "alert alert-warning"
+      end
+  end
 end
