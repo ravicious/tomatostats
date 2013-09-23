55a5e40c9f17b0da451228a0d4720e98ef769aad
Rafał Cieślak <ravicious@gmail.com>
2013-09-23 15:08:49 +0200

    Add glyph helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       yield
     end
   end
+
+  def glyph(name, white: false, space: true)
+    content_tag(:i, nil, class: "icon-#{name}#{" icon-white" if white}") + (space ? " " : "")
+  end
 end
