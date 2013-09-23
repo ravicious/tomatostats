9b884a1cc6bd34aee0797524140b214a31f757cd
Rafał Cieślak <ravicious@gmail.com>
2013-09-23 15:38:11 +0200

    Remove sidebar code from ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,4 @@
 module ApplicationHelper
-  def sidebar(title: false, &block)
-    content_tag(:div, class: 'panel panel-default') do
-      sidebar_title(title) + sidebar_body{yield}
-    end
-  end
-
-  def sidebar_title(title)
-    if title
-      content_tag(:div, class: 'panel-heading') do
-        content_tag(:h3, title, class: 'panel-title')
-      end
-    else
-      "".html_safe
-    end
-  end
-
-  def sidebar_body(&block)
-    content_tag(:div, class: 'panel-body') do
-      yield block
-    end
-  end
-
   def glyph(name, white: false, space: true)
     content_tag(:i, nil, class: "icon-#{name}#{" icon-white" if white}") + (space ? " " : "")
   end
