e7146aa64c905542f90e5ea60c3812c158308be9
Rafał Cieślak <ravicious@gmail.com>
2013-10-03 16:33:19 +0200

    Spec for Pomodoro.stats

--- spec/models/pomodoro_spec.rb
+++ spec/models/pomodoro_spec.rb
@@ -10,4 +10,44 @@ describe Pomodoro do
       expect(invalid_pomodoro).not_to be_valid
     end
   end
+
+  describe ".stats" do
+    before do
+      # destroy that subject created before
+      Pomodoro.destroy_all
+
+      3.times { |m|
+        30.times { |day_count|
+          create(:pomodoro,
+                 started_at: m.months.ago.beginning_of_month.to_i + day_count*86400 + 60*30,
+                 finished_at: m.months.ago.beginning_of_month.to_i + day_count*86400 + 60*30 + 25*60
+                )
+        }
+      }
+    end
+
+    subject { Pomodoro.stats }
+
+    it "calculates total number of pomodoros" do
+      expect(subject['count']).to eq(90)
+    end
+
+    it "makes one group per month" do
+      expect(subject['months']).to have(3).groups
+    end
+
+    it "makes one group per week" do
+      subject['months'].each_value do |month|
+        expect(month['weeks']).to have_at_least(4).groups
+      end
+    end
+
+    it "makes one group per day" do
+      subject['months'].each_value do |month|
+        month['weeks'].each_value do |week|
+          expect(week['days']).to have_at_least(1).group
+        end
+      end
+    end
+  end
 end
