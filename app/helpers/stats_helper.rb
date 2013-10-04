0111d486efc22a57adb56ed41ffb10bf48bbf988
Rafał Cieślak <ravicious@gmail.com>
2013-10-04 12:18:42 +0200

    Add pomodoros_count helper

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -4,4 +4,10 @@ module StatsHelper
     end_date = Date.parse(last_date).to_s(:short)
     "#{start_date} - #{end_date}"
   end
+
+  def pomodoros_count(count)
+    content_tag(:span, class: 'pomodoros-count') do
+      pluralize(count, "pomodoro")
+    end
+  end
 end
