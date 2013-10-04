3133ae9e17ceeb97d4867b30d751d3430915e91f
Rafał Cieślak <ravicious@gmail.com>
2013-10-04 12:08:16 +0200

    Rename DateHelper to StatsHelper

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,4 +1,4 @@
-module DateHelper
+module StatsHelper
   def days_range(first_date, last_date)
     start_date = Date.parse(first_date).to_s(:short)
     end_date = Date.parse(last_date).to_s(:short)
