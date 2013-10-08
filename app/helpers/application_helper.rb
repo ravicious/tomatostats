c5168c241494095221bc3af1124ccafa9a905a41
Rafał Cieślak <ravicious@gmail.com>
2013-10-08 16:34:55 +0200

    Added missing else clause

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,12 @@ module ApplicationHelper
   end
 
   def flash_class(level)
-      case level
-          when :notice then "alert alert-info"
-          when :success then "alert alert-success"
-          when :error then "alert alert-warning"
-          when :alert then "alert alert-warning"
-      end
+    case level
+    when :notice then "alert alert-info"
+    when :success then "alert alert-success"
+    when :error then "alert alert-warning"
+    when :alert then "alert alert-warning"
+    else "alert alert-info"
+    end
   end
 end
