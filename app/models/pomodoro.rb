3f23332ef9bd68ae7f6b61abe7b7cf013cd93691
Rafał Cieślak <ravicious@gmail.com>
2013-10-08 15:44:45 +0200

    Remove old code

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -1,11 +1,11 @@
 class Pomodoro < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
   validate :duration_cannot_be_longer_than_1_hour
   validates_uniqueness_of :started_at, :finished_at, scope: :user
   validates_numericality_of :started_at, :finished_at
 
-  # TODO: rewrite this method
   def duration
     Time.diff Time.at(started_at), Time.at(finished_at), '%N %S'
   end
@@ -42,34 +42,6 @@ class Pomodoro < ActiveRecord::Base
     raw_days = group("to_char(to_timestamp(started_at), 'YYYY-MM-IW-DD')").count.sort
 
     stats = StatsProcessor::Stats.new(raw_months, raw_weeks, raw_days).parse
-  end
-
-  def self.done_weeks_ago(number_of)
-    self.done_some_time_ago(number_of, 'week')
-  end
-
-  def self.done_months_ago(number_of)
-    self.done_some_time_ago(number_of, 'month')
-  end
-
-  def self.done_this_week
-    self.done_weeks_ago(0)
-  end
-
-  def self.done_this_month
-    self.done_months_ago(0)
-  end
-
-  def has_project?
-    project_id
-  end
-
-  private
-
-  def self.done_some_time_ago(number_of, time_range_type)
-    beginning_of_time_range_in_seconds = eval("#{number_of}.#{time_range_type}s.ago.to_date." +
-                                              "at_beginning_of_#{time_range_type}.to_time.to_i")
-    where("started_at >= ?", beginning_of_time_range_in_seconds)
   end
 
 end
